c2e9dfe72d36a9cede5cbf284e92aa7dfe5f79ee
Ryan <rgwbarnes@gmail.com>
2014-02-08 19:17:23 -0800

    added validations code

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,7 @@
 class Listing < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates_attachment_presence :image
 end
